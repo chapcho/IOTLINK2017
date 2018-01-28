59ffbbe6c4ec286dc2f0fb217de732b5f754ee02
chapcho <chapcho69@gmail.com>
2018-01-29 05:49:43 +0900

    데이터 패킷 일반화 구현중. 패킷 데이터와 패킷 클래스 구현중

---
+++ IOTL/IOTL.Common/IOTL.Common/Entity/CPacketData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTL.Common.Entity
+{
+    public class CPacketData
+    {
+        private int m_iPositionInPacket;
+        private int m_iPacketDataLength;
+        private string m_sDataType;
+        private string m_sValue;
+
+        public CPacketData()
+        {
+
+        }
+
+        public int PositionInPacket
+        {
+            get { return m_iPositionInPacket; }
+            set { m_iPositionInPacket = value; }
+        }
+
+        public int DataLengthInPacket
+        {
+            get { return m_iPacketDataLength; }
+            set { m_iPacketDataLength = value; }
+        }
+
+        public string DataTypeDesc
+        {
+            get { return m_sDataType; }
+            set { m_sDataType = value; }
+        }
+
+        public string DataValue
+        {
+            get { return m_sValue; }
+            set { m_sValue = value; }
+        }
+    }
+}
---
+++ IOTL/IOTL.Common/IOTL.Common/Entity/CPacketDefine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTL.Common.Entity
+{
+    public class CPacketDefine : List<CPacketData>
+    {
+        private string m_sPacketName;
+        private string m_sPacketDesc;
+        private string m_sPacketStartIndicater;
+        private string m_sPacketEndIndicater;
+        private int m_iPacketMemberCount;
+        private int m_iReceiveCount;
+        private DateTime m_dLastReceiveDt;
+
+        public CPacketDefine()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            this.Clear();
+        }
+    }
+}
